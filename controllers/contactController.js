2e90eda62e03994f3806bb7132ae935e953e96d6
manojLovesCoding <mkum9417@gmail.com>
2024-11-17 19:22:02 +0530

    the logged in user,updates and delete the contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,7 @@ const Contact = require("../models/contactModel");
 //@route GET /api/contacts
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find({ user_id: req.user_id });
+    const contacts = await Contact.find({ user_id: req.user.id });
     res.status(200).json(contacts);
 });
 
@@ -51,6 +51,11 @@ const updateContact = asyncHandler(async (req, res) => {
         throw new Error("contact not found");
     }
 
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to update other contact")
+    }
+
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
@@ -70,7 +75,12 @@ const deleteContact = asyncHandler(async (req, res) => {
         throw new Error("contact not found");
     }
 
-    await Contact.deleteOne();
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to delete other contact")
+    }
+
+    await Contact.deleteOne({ _id: req.params.id });
 
     res.status(200).json(contact);
 });
