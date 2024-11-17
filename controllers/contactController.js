70cb7e50dea5e7e5203b0eaa639aa3764841ad7c
manojLovesCoding <mkum9417@gmail.com>
2024-11-17 18:02:42 +0530

    protecting contact routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,7 +3,7 @@ const Contact = require("../models/contactModel");
 
 //desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
     const contacts = await Contact.find();
     res.status(200).json(contacts);
@@ -11,7 +11,7 @@ const getContacts = asyncHandler(async (req, res) => {
 
 //desc Get all contacts
 //@route POST /api/contacts
-//@access public
+//@access private
 const CreateContact = asyncHandler(async (req, res) => {
     console.log("The request body is :", req.body);
 
@@ -28,7 +28,7 @@ const CreateContact = asyncHandler(async (req, res) => {
 
 //desc Get contact
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -40,7 +40,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 //desc Update contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -59,7 +59,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 //desc Delete contact
 //@route DELETE /api/contacts/:id
-//@access public
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { CreateContact, getContacts, getContact, updateContact, deleteContact } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
 
+router.use(validateToken);
 router.route("/").get(getContacts).post(CreateContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);;
 module.exports = router;
+
