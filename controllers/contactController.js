55c72822aae6c0b9a420c2eedfeba37d77a2bcdf
manojLovesCoding <mkum9417@gmail.com>
2024-11-17 10:00:19 +0530

    created middleware to handle error

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,6 +9,14 @@ const getContacts = ((req, res) => {
 //@route POST /api/contacts
 //@access public
 const CreateContact = ((req, res) => {
+    console.log("The request body is :", req.body);
+
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
     res.status(200).json({ message: "Create Contacts" });
 });
 
